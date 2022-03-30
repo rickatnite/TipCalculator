12f41bafbfa189142e28eab0afd557d3dc28f9ee
ric <eroche1@student.gsu.edu>
2022-03-30 13:01:06 -0400

    made buttons and slider clickable without crash

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -13,27 +13,36 @@ class ViewController: UIViewController {
     @IBOutlet weak var outputTipTotal: UILabel!
     
     @IBAction func button15(_ sender: Any) {
-        let bill = Double(billInput.text!)!
-        let tip15 = bill * 0.15
-        let total15 = bill + tip15
-        let output = "Tip: $\(tip15), Total: $\(total15)"
-        outputTipTotal.text = output
+        var bill:Double! = nil
+        bill = Double(billInput.text ?? "0")
+        if bill != nil {
+            let tip15 = round((bill! * 0.15) * 100) / 100.00
+            let total15 = round((bill! + tip15) * 100) / 100.00
+            let output = "Tip: $\(tip15), Total: $\(total15)"
+            outputTipTotal.text = output
+        }
     }
     
     @IBAction func button18(_ sender: Any) {
-        let bill = Double(billInput.text!)!
-        let tip18 = bill * 0.18
-        let total18 = bill + tip18
-        let output = "Tip: $\(tip18), Total: $\(total18)"
-        outputTipTotal.text = output
+        var bill:Double! = nil
+        bill = Double(billInput.text ?? "0")
+        if bill != nil {
+            let tip18 = round((bill * 0.18) * 100) / 100.00
+            let total18 = round((bill + tip18) * 100) / 100.00
+            let output = "Tip: $\(tip18), Total: $\(total18)"
+            outputTipTotal.text = output
+        }
     }
     
     @IBAction func button20(_ sender: Any) {
-        let bill = Double(billInput.text!)!
-        let tip20 = bill * 0.2
-        let total20 = bill + tip20
-        let output = "Tip: $\(tip20), Total: $\(total20)"
-        outputTipTotal.text = output
+        var bill:Double! = nil
+        bill = Double(billInput.text ?? "0")
+        if bill != nil {
+            let tip20 = round((bill * 0.2) * 100) / 100.00
+            let total20 = round((bill + tip20) * 100) / 100.00
+            let output = "Tip: $\(tip20), Total: $\(total20)"
+            outputTipTotal.text = output
+        }
     }
     
     @IBOutlet weak var sliderValue: UISlider!
@@ -41,12 +50,14 @@ class ViewController: UIViewController {
     
     @IBAction func tipSlider(_ sender: Any) {
         let bill = Float(billInput.text!)
-        let tip = round((bill! * sliderValue.value) * 100) / 100.00
-        let tipAmt = Int(sliderValue.value * 100)
-        let total = round((bill! + tip) * 100) / 100.00
-        let output = "Tip: $\(tip), Total: $\(total)"
-        outputTipTotal.text = output
-        tipAmount.text = "\(tipAmt)%"
+        if bill != nil {
+            let tip = round((bill! * sliderValue.value) * 100) / 100.00
+            let tipAmt = Int(sliderValue.value * 100)
+            let total = round((bill! + tip) * 100) / 100.00
+            let output = "Tip: $\(tip), Total: $\(total)"
+            outputTipTotal.text = output
+            tipAmount.text = "\(tipAmt)%"
+        }
     }
     
     override func viewDidLoad() {
